2c597bed2da1d8606d1fcd0347c3910f812a4c90
Sajjad <sajjadmazharjr@gmail.com>
2023-10-02 12:43:41 +0530

    fix bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ export const register = async (req, res) => {
       lastName,
       email,
       password: passwordHash,
-      picturePath,
+      picturePath:req.file.path,
       friends,
       location,
       occupation,
